27b0017600255943dacc64ddb653796e9e7392f7
CoNickel-MC <springterror228@gmail.com>
2024-02-21 09:53:27 +0530

    Add project files.

---
+++ PongGame.cpp
@@ -0,0 +1,16 @@
+// PongGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "raylib.h"
+
+int main(){
+	const int winWidth = 800;
+	const int winHeight = 550;
+
+	InitWindow(winWidth, winHeight, "PongGame");
+	while (!WindowShouldClose) {
+
+	}
+	CloseWindow();
+	return 0;
+}
