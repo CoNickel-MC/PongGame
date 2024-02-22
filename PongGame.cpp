c5cac2839379b70dafe37f36fd54ea0c398dd58c
CoNickel-MC <springterror228@gmail.com>
2024-02-22 08:05:52 +0530

    Added ball object Moving Paddle Background

--- PongGame.cpp
+++ PongGame.cpp
@@ -1,15 +1,62 @@
-// PongGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "raylib.h"
 
-int main(){
+class ball {
+	private:
+		int ballXVel;int ballYVel;
+		int collisions;
+	public:
+		int ballX;
+		int ballY;
+		void ballCollision(char collidedWith) {
+			if (collidedWith == 'u') {
+
+			}
+			else {
+
+			}
+
+		};
+		void ballMove() {
+
+		};
+};
+void CollisionCheck() {
+
+};
+int main() {
 	const int winWidth = 800;
 	const int winHeight = 550;
+	float userY = 20.0f;
+	float compY = 20.0f;
+	Rectangle userPosition;
+	Rectangle CompPosition;
+	Rectangle BallPosition;
+
 
 	InitWindow(winWidth, winHeight, "PongGame");
-	while (!WindowShouldClose) {
+	SetTargetFPS(60);
 
+
+
+	while (!WindowShouldClose()) {
+		if (IsKeyDown(KEY_S) && userY<(winHeight-80)) {userY += 2.5f;}
+		if (IsKeyDown(KEY_W) &&  userY>(5)) {userY -= 2.5f;}
+
+
+		userPosition = {5, userY, 15, 75};
+		CompPosition = {winWidth - 20, compY, 15, 75};
+		Camera2D camera = {0};
+		camera.offset = { winWidth / 2.0f, winHeight / 2.0f };
+		camera.rotation = 0.0f;
+		camera.zoom = 1.0f;
+		camera.target = { 0,0 };
+
+		BeginDrawing();
+		ClearBackground(RED);
+		DrawRectangleRec({ 2,2,396,546 }, SKYBLUE);
+			DrawRectangleRounded(userPosition, 0.5f, 20.0f, BLUE);					//User paddle
+			DrawRectangleRounded(CompPosition, 0.5f, 20.0f, DARKBLUE);				//Comp paddle
+		EndDrawing();
 	}
 	CloseWindow();
 	return 0;
